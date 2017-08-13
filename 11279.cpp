c98d96be3652e45c567f813167cc67574633cf37
csh8130 <csh8130@naver.com>
2017-08-14 02:31:54 +0900

    maxHeap 문제 (우선순위 큐 연산자 오버로딩으로 해결)
    maxHeap 문제 (우선순위 큐 연산자 오버로딩으로 해결)

---
+++ 11279.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+priority_queue<int> q;
+int n;
+int main()
+{
+	//freopen("input.txt", "r", stdin);
+
+	cin >> n;
+	int temp;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> temp;
+		
+		if (temp == 0)
+		{
+			if (q.empty())
+			{
+				cout << 0 << endl;
+			}
+			else
+			{
+				cout << q.top() << endl;
+				q.pop();
+			}
+		}
+		else
+			q.push(temp);
+	}
+}
